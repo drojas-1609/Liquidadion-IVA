e8a6735fa086974fbc95e218b8a4d0d51b8bba0a
Diego Rojas <diegorojas@MacBook-Pro-de-Diego.local>
2026-08-24 16:13:59 -0300

    fix prisma export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-    return new PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.DATABASE_URL,
-            },
-        },
-    } as any);
+    return new PrismaClient();
 };
 
 declare global {
     var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-export default prisma;
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+
+export default prisma;
